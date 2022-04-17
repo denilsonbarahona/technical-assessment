86a2deac472e65f5fb85e3eaeb833ac909091e74
denilsonbarahona <91sbarahona@gmail.com>
2022-04-17 17:27:38 -0600

    [add] commet for the redirect file

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,2 +1,5 @@
+/**
+ * code to create the _redirect file in the dist folder in order to handle routes for SPA in netlify
+ */
 const fs = require("fs");
 fs.writeFileSync("./dist/_redirects", `/*    /index.html  200`)
